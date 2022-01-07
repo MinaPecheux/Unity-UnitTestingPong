a1644da94b5433932a13cb3835a4292f8ff94281
remarkov <remarkov@gmail.com>
2022-01-07 15:59:11 +0100

    revert windows-related changes

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -19,20 +19,6 @@ public static class BuildScript
         Debug.Log("Built StandaloneOSX");
     }
 
-    [MenuItem("Build/Build Windows")]
-    public static void BuildWindows()
-    {
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.locationPathName = "Build/" + Application.productName + ".exe";
-        buildPlayerOptions.target = BuildTarget.StandaloneOSX;
-        buildPlayerOptions.options = BuildOptions.None;
-        buildPlayerOptions.scenes = GetScenes();
-
-        Debug.Log("Building StandaloneOSX");
-        BuildPipeline.BuildPlayer(buildPlayerOptions);
-        Debug.Log("Built StandaloneOSX");
-    }
-
     private static string[] GetScenes()
     {
         return (from scene in EditorBuildSettings.scenes where scene.enabled select scene.path).ToArray();
