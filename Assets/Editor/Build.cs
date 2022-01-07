acd5d68c0e498966f1332e1abd0c49ac6a33b971
remarkov <remarkov@gmail.com>
2022-01-07 13:42:12 +0100

    try windows

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -9,7 +9,21 @@ public static class BuildScript
     public static void BuildMac()
     {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.locationPathName = "mac/" + Application.productName + ".app";
+        buildPlayerOptions.locationPathName = "Build/" + Application.productName + ".app";
+        buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+        buildPlayerOptions.options = BuildOptions.None;
+        buildPlayerOptions.scenes = GetScenes();
+
+        Debug.Log("Building StandaloneOSX");
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+        Debug.Log("Built StandaloneOSX");
+    }
+
+    [MenuItem("Build/Build Windows")]
+    public static void BuildWindows()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.locationPathName = "Build/" + Application.productName + ".exe";
         buildPlayerOptions.target = BuildTarget.StandaloneOSX;
         buildPlayerOptions.options = BuildOptions.None;
         buildPlayerOptions.scenes = GetScenes();
